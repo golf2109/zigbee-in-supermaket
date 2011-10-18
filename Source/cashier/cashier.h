55194703f55f8ea317cd981d13fd0d4d43366731
nguyenquvinh <nguyenquvinh@gmail.com>
2011-10-18 04:31:09 +0000

    * delete old Source * update new framework

---
+++ Source/cashier/cashier.h
@@ -0,0 +1,6 @@
+#ifndef CASHIER_H
+#define CASHIER_H
+
+/*CODE HERE*/
+
+#endif
---
+++ Source/common.c
@@ -0,0 +1,48 @@
+/*********************************************************************
+ * INCLUDES
+ */
+
+/*USER CODE*/
+#include "common.h"
+
+/*********************************************************************
+ * SECURITY
+ */
+
+/*********************************************************************
+ * CONNECT TO PC
+ */
+ 
+ /*********************************************************************
+ * SERIAL
+ */
+ 
+/*********************************************************************
+ * ZIGBEE
+ */
+ 
+ /*********************************************************************
+ * GLOBAL FUNCTIONS
+ */
+#if defined( LCD_SUPPORTED )
+void printText(char* text1, char* text2){
+  HalLcdWriteScreen(text1, text2);
+}
+#endif
+
+char* int2char(int num){
+  unsigned char i=0,j=0;
+  int tmp = num;
+  while(1){
+    tmp = tmp/10;
+    i++;
+    if(tmp<=0) break;
+  }
+  char* ret = (char*)osal_mem_alloc(sizeof(char)*i);
+  for(j=i;j>0;j--){
+    *(ret+j) = num%10 + 48;
+    num=num/10;
+  }
+  *(ret+j) = '\0';
+  return ret;
+}
