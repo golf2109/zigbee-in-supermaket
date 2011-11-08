bcf147a9c146d4c0076ef2a04aa1c9961141bec4
hainguyen.8y@gmail.com <hainguyen.8y@gmail.com@e8f5e717-723e-bfbc-8633-9ab81850cf36>
2011-11-08 02:47:50 +0000

    Modified GUI look like OS environment

--- trunk/PCApplication/Queue-Bursting/src/com/Gui/MainGui.java
+++ trunk/PCApplication/Queue-Bursting/src/com/Gui/MainGui.java
@@ -28,6 +28,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.BevelBorder;
 
 import jxl.write.WriteException;
@@ -98,6 +100,23 @@ public class MainGui extends JFrame {
 	 */
 	public MainGui() {
 		super();
+		try {
+			// Set System L&F
+			UIManager.setLookAndFeel(
+			UIManager.getSystemLookAndFeelClassName());
+			} 
+			catch (UnsupportedLookAndFeelException e) {
+			// handle exception
+			}
+			catch (ClassNotFoundException e) {
+			// handle exception
+			}
+			catch (InstantiationException e) {
+			// handle exception
+			}
+			catch (IllegalAccessException e) {
+			// handle exception
+			}
 		initialize();
 		
 	}
